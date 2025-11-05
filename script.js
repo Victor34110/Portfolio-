dca01ad2d373be2247818254b46167c3f0025461
Victor Galian <vic.galian22@gmail.com>
2025-11-05 01:48:55 +0100

    beaucoup de changement

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+ },
+  });
+});
