d3eb4b5662890eb5eb39c34c63232ff1363c47a9
Victor Galian <vic.galian22@gmail.com>
2025-11-16 02:33:07 +0100

    responsive tablette/ grand telephone

--- script.js
+++ script.js
@@ -12,4 +12,13 @@ const swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
  },
   });
+
+   // Menu responsive
+   
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
 });
